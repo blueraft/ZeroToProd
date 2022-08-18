acf93f97d8beb469a18d3e77e2d941deeb16a5a0
ahmedilyas <ilyas@campus.tu-berlin.de>
2022-08-19 00:41:45 +0200

    tracing

--- src/startup.rs
+++ src/startup.rs
@@ -1,5 +1,6 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::middleware::Logger;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
@@ -10,6 +11,7 @@ pub fn run(listener: TcpListener, connection: PgPool) -> Result<Server, std::io:
     // Capture `connection` from the surrounding enviroment
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
             // Get a pointer copy and attach it to the app state
