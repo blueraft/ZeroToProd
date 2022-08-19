ad3c08f7c69b3ca3b97ef5671fad77a66e587c43
ahmedilyas <ilyas@campus.tu-berlin.de>
2022-08-19 13:49:15 +0200

    decorate subscribe fns using tracing::instrument

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,10 @@
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use once_cell::sync::Lazy;
 
 // Ensure that the `tracing` stack is only initialised once using `once_cell`
 static TRACING: Lazy<()> = Lazy::new(|| {
