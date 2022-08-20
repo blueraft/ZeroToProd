d8c2ae061dffcc4112aef5e456838cf8d7288551
ahmedilyas <ilyas@campus.tu-berlin.de>
2022-08-20 12:53:34 +0200

    Dockerfile & config changes

--- src/configuration.rs
+++ src/configuration.rs
@@ -5,7 +5,7 @@ use serde;
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -17,11 +17,21 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    let enviroment: Enviroment = std::env::var("APP_ENVIROMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIROMENT");
+    let enviroment_filename = format!("{}.yaml", enviroment.as_str());
     //initialize
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
+        .add_source(config::File::from(
+            configuration_directory.join("base.yaml"),
+        ))
+        .add_source(config::File::from(
+            configuration_directory.join(&enviroment_filename),
         ))
         .build()?;
     settings.try_deserialize::<Settings>()
@@ -46,5 +56,40 @@ impl DatabaseSettings {
             self.host,
             self.port
         ))
+    }
+}
+
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+pub enum Enviroment {
+    Local,
+    Production,
+}
+
+impl Enviroment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Enviroment::Local => "local",
+            Enviroment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Enviroment {
+    type Error = String;
+
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a suppoerted enviroment. Use either `local` or `production`.",
+                other
+            )),
+        }
     }
 }
